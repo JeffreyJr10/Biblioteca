09d220097ab0f273e4665cc1e0544f6ceed8b914
JeffreyJr10 <112107497+JeffreyJr10@users.noreply.github.com>
2022-08-25 12:34:51 -0500

    modificar

--- Biblioteca/Program.cs
+++ Biblioteca/Program.cs
@@ -14,6 +14,7 @@ namespace Biblioteca
         [STAThread]
         static void Main()
         {
+            Console.WriteLine = "HoOlaaa MundoOOO..!!";
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
